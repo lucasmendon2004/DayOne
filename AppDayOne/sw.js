b3fa9f1f932959830f127ed5510a46563c4299fb
lucasmendon2004 <lucassmendon2004@gmail.com>
2026-09-08 12:16:50 -0300

    Add files via upload

---
+++ AppDayOne/sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "dayone-v1";
+
+const FILES = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json"
+]
+
+
+self.addEventListener(
+    "install",
+    event => {
+
+        event.waitUntil(
+
+            caches.open(CACHE_NAME)
+                .then(cache =>
+                    cache.addAll(FILES)
+                )
+
+        )
+
+    }
+)
+
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        event.respondWith(
+
+            caches.match(event.request)
+                .then(response => {
+
+                    return response ||
+                        fetch(event.request);
+
+                })
+
+        )
+
+    }
+)
